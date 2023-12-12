00d36eaf475c9333d4639e56885717626bc0caf0
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-12 17:55:22 +0600

    to lower and upper case

--- search.js
+++ search.js
@@ -5,5 +5,15 @@ const lyrics = 'Zara Sa Dil Main';
 // const doesExist = lyrics.includes('Zara');
 // or,
 const searchString = 'Zara';
-const doesExist = lyrics.includes(searchString);
-console.log(doesExist);
+// const doesExist = lyrics.includes(searchString);
+
+// toLowerCase()
+const lyricsLowerCase = lyrics.toLowerCase();
+const doesExist = lyricsLowerCase.includes(searchString);
+// console.log(doesExist);
+
+// toUpperCase()
+const write = 'MAIN';
+const lyricsUpperCase = write.toUpperCase();
+const exist = lyricsUpperCase.includes(write);
+console.log(exist);
