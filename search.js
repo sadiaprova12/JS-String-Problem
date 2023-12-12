4f74725a6a5b0139fc16249249441feda374e8c8
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-12 17:45:19 +0600

    search string

--- search.js
+++ search.js
@@ -2,5 +2,8 @@ const lyrics = 'Zara Sa Dil Main';
 // console.log(lyrics);
 
 // true or false. j variable dibo seta ase kina bujhar khuje bher korar process:
-const doesExist = lyrics.includes('Zara');
+// const doesExist = lyrics.includes('Zara');
+// or,
+const searchString = 'Zara';
+const doesExist = lyrics.includes(searchString);
 console.log(doesExist);
