1914d6ac761b083e73b1e2d3eeaf0c04d3cb2bbb
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-12 17:08:50 +0600

    to upper case added

--- case.js
+++ case.js
@@ -2,6 +2,8 @@ const userName = 'blackPink';
 const userInput = 'blackpink';
 // toLowerCase() deyar reason hocche joto letter ase sob choto word hoye jabe. For Example : 'blackPink' Output hbe: blackpink. Majher P ta choto word hoye jabe .
 console.log(userName.toLowerCase());
+// toUpperCase() deyar reason hocche joto letter ase sob boro word hoye jabe. For Example : 'blackPink' Output hbe: BLACKPINK.
+console.log(userInput.toUpperCase());
 if( userName === userInput){
     console.log('valid user')
 }
