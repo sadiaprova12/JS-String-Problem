73ddb625ee46f6ae565eaae36070d81e0b69ba35
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-12 17:18:15 +0600

    does exist includes

---
+++ search.js
@@ -0,0 +1,6 @@
+const lyrics = 'Zara Sa Dil Main';
+// console.log(lyrics);
+
+// true or false. j variable dibo seta ase kina bujhar khuje bher korar process:
+const doesExist = lyrics.includes('Zara');
+console.log(doesExist);
