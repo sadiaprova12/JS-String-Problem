97fdbdbb8f6685e973fbe2828fd7dba8a679742c
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-12 17:06:37 +0600

    to lower case added

--- case.js
+++ case.js
@@ -1,6 +1,7 @@
 const userName = 'blackPink';
 const userInput = 'blackpink';
-
+// toLowerCase() deyar reason hocche joto letter ase sob choto word hoye jabe. For Example : 'blackPink' Output hbe: blackpink. Majher P ta choto word hoye jabe .
+console.log(userName.toLowerCase());
 if( userName === userInput){
     console.log('valid user')
 }
