862cd546571c627fef0827fe295fc8ab16bd12f2
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-13 23:12:10 +0600

    added characters

--- split-join.js
+++ split-join.js
@@ -1,5 +1,7 @@
 const lyrics = 'Zara Sa Dil Main';
 const parts = lyrics.split(' ');
 const sentences = lyrics.split('.');
+const chars = lyrics.split('');
 // console.log(parts);
-console.log(sentences);
+// console.log(sentences);
+console.log(chars);
