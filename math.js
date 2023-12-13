315172577c0e92edf964ce26cbe18ca5e2481f9c
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-13 23:33:43 +0600

    added math

---
+++ math.js
@@ -0,0 +1,14 @@
+const result = Math.pow(3, 8); // pow mane hocche power. mane 2 the power 6 somthing bole thaki seta bujhano hoy.
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = num1 - num2;
+console.log(gap);
+if(gap < 5){
+    console.log('You guys can be friends');
+}
+else{
+    console.log('You guys stay a part');
+}
