58e594a0897c380994eeb2257e2104d89d7beafe
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-17 22:04:48 +0600

    approch one and two

--- swap-temp.js
+++ swap-temp.js
@@ -1,9 +1,19 @@
 let first = 5;let second = 7;
 // console.log(first, second);
 
+// this is wrong approch
 // first = second;
 // second = first;
+// console.log(first,second);
+
+// Approch: 1
 const temp = first;
 first = second;
 second = temp;
-console.log(first,second);
+// console.log(first,second);
+
+// Approch: 2
+
+[first, second] = [second, first];
+
+console.log(first, second);
