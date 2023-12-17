783cc4e4ba3da3dae27b54763198823fed7c5e9a
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-17 22:26:03 +0600

    find out who will get delicious cake

---
+++ max3.js
@@ -0,0 +1,13 @@
+const jim = 69;
+const dela = 97;
+const chinku = 97;
+
+if(jim > dela && jim > chinku){
+        console.log('Jim will get the cake');
+}
+else if(dela > jim && dela > chinku){
+    console.log('Dela will eat cake on this dela');
+}
+else{
+    console.log('chinku will get the cake');
+}
