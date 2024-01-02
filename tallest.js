ef2a3e1823aa3a2f11f5af6de427ce6190c683cd
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-02 20:09:43 +0600

    added numbers element

--- tallest.js
+++ tallest.js
@@ -1,5 +1,10 @@
 function maxInArray(numbers){
-
+//  console.log('Array inside the array' , numbers);
+for(let i = 0; i < numbers.length; i++){
+    const index = i;
+    const element = numbers[index];
+    console.log(element);
+}
 }
 
 const heights =[167, 190, 120, 165, 137];
