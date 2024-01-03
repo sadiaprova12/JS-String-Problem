0fe83415cbb6bcee13de434b9626881f27acf5e4
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 01:49:22 +0600

    added reverse js

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function reverseString(text){
+    for(let i = 0; i< text.length; i++){
+        const element = text[i];
+        console.log(element);
+    }
+}
+
+const myString = 'I am a good boy';
+const reversed = reverseString(myString);
