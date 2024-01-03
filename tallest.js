d256e500fd160794aa34b5f12028503dfe94cff1
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-03 23:58:02 +0600

    comment

--- tallest.js
+++ tallest.js
@@ -1,9 +1,14 @@
 function maxInArray(numbers){
 //  console.log('Array inside the array' , numbers);
 for(let i = 0; i < numbers.length; i++){
+    // let largest = numbers[0];
     const index = i;
     const element = numbers[index];
-    console.log(element);
+    // console.log(element);
+    if(element > largest){
+        largest = element;
+    }
+    return largest;
 }
 }
 
