94c6391f3118d75d7f7e47ffe5cb99de7a490374
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 00:25:15 +0600

    added task

--- tallest.js
+++ tallest.js
@@ -15,3 +15,5 @@ for(let i = 0; i < numbers.length; i++){
 const heights =[167, 190, 120, 165, 137];
 const tallest = maxInArray(heights);
 console.log('Tallest Person is: ',tallest);
+
+// Task: Write a function to get the lowest number in an array
