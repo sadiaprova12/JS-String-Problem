5d5a755574bc58cc7679b38f03705b43ffc6a1e4
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 18:00:24 +0600

    my string word all rotated

--- word-reverse.js
+++ word-reverse.js
@@ -1,6 +1,13 @@
 function reverseWords(str){
     const words = str.split(' ');
-    console.log(words);
+    // console.log(words);
+    const result = [];
+    // rotated or reverse words
+    for(let i = words.length - 1; i >= 0; i--){
+        const element = words[i];
+        result.push(element);
+    }
+    console.log(result);
 }
 
 
